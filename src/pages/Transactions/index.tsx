3b9bcf5f01c3820f065068977c4b890eafbf45c6
crystian97 <icrystianveloso@gmail.com>
2023-01-27 21:36:47 -0300

    feat:Tabela de transações

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,11 +1,38 @@
 import { Header } from "../../Components/Header";
 import { Summary } from "../../Components/Summary";
+import {
+  PriceHighLight,
+  TransactionsContainer,
+  TransactionsTable,
+} from "./styles";
 
 export function Transactions() {
   return (
     <div>
       <Header />
       <Summary />
+      <TransactionsContainer>
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td width="50%">Desenvolvimento de site</td>
+              <td>
+                <PriceHighLight variant="income">R$ 12.000,00</PriceHighLight>
+              </td>
+              <td>Venda</td>
+              <td>13/04/2022</td>
+            </tr>
+            <tr>
+              <td width="50%">Hamburguer</td>
+              <td>
+                <PriceHighLight variant="outcome"> -R$ 50,00</PriceHighLight>
+              </td>
+              <td>Alimentação</td>
+              <td>10/04/2022</td>
+            </tr>
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
     </div>
   );
 }
