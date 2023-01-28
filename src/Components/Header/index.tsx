c2b008aa7e420e03c01268c3d35f732aed617f86
crystian97 <icrystianveloso@gmail.com>
2023-01-27 22:32:15 -0300

    feat:Criando modal acessível

--- src/Components/Header/index.tsx
+++ src/Components/Header/index.tsx
@@ -1,11 +1,24 @@
 import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
 import iLogo from "../../assets/img/logo.svg";
+import * as Dialog from "@radix-ui/react-dialog";
+
 export function Header() {
   return (
     <HeaderContainer>
       <HeaderContent>
         <img src={iLogo} />
-        <NewTransactionButton>Nova transação</NewTransactionButton>
+        <Dialog.Root>
+          <Dialog.Trigger asChild>
+            <NewTransactionButton>Nova transação</NewTransactionButton>
+          </Dialog.Trigger>
+          <Dialog.Portal>
+            <Dialog.Overlay />
+            <Dialog.Content>
+              <Dialog.Title>Nova Transação</Dialog.Title>
+              <Dialog.Close />
+            </Dialog.Content>
+          </Dialog.Portal>
+        </Dialog.Root>
       </HeaderContent>
     </HeaderContainer>
   );
