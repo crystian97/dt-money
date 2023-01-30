f659dda00207276dc6983a64fb48ebaef75bfa55
crystian97 <icrystianveloso@gmail.com>
2023-01-30 14:06:41 -0300

    feat:finish close button

--- src/Components/NewTransactionModal/index.tsx
+++ src/Components/NewTransactionModal/index.tsx
@@ -1,5 +1,6 @@
 import * as Dialog from "@radix-ui/react-dialog";
-import { Content, Overlay } from "./styles";
+import { X } from "phosphor-react";
+import { CloseButton, Content, Overlay } from "./styles";
 
 export function NewTransactionModal() {
   return (
@@ -7,7 +8,17 @@ export function NewTransactionModal() {
       <Overlay />
       <Content>
         <Dialog.Title>Nova Transação</Dialog.Title>
-        <Dialog.Close />
+        <CloseButton>
+          <X />
+        </CloseButton>
+
+        <form action="">
+          <input type="text" placeholder="Descrição" required />
+          <input type="number" placeholder="Preço" required />
+          <input type="text" placeholder="Categoria" required />
+
+          <button type="submit">Cadastrar</button>
+        </form>
       </Content>
     </Dialog.Portal>
   );
