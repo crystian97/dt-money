7c56a8ca060f4dfd5a48379473ddab8d68a6866a
crystian97 <icrystianveloso@gmail.com>
2023-01-30 14:18:34 -0300

    feat:Botões de transação

--- src/Components/NewTransactionModal/index.tsx
+++ src/Components/NewTransactionModal/index.tsx
@@ -1,6 +1,12 @@
 import * as Dialog from "@radix-ui/react-dialog";
-import { X } from "phosphor-react";
-import { CloseButton, Content, Overlay } from "./styles";
+import { ArrowCircleDown, ArrowCircleUp, X } from "phosphor-react";
+import {
+  CloseButton,
+  Content,
+  Overlay,
+  TransactionType,
+  TransactionTypeButton,
+} from "./styles";
 
 export function NewTransactionModal() {
   return (
@@ -17,6 +23,16 @@ export function NewTransactionModal() {
           <input type="number" placeholder="Preço" required />
           <input type="text" placeholder="Categoria" required />
 
+          <TransactionType>
+            <TransactionTypeButton>
+              <ArrowCircleUp />
+              Entrada
+            </TransactionTypeButton>
+            <TransactionTypeButton>
+              <ArrowCircleDown />
+              Saída
+            </TransactionTypeButton>
+          </TransactionType>
           <button type="submit">Cadastrar</button>
         </form>
       </Content>
