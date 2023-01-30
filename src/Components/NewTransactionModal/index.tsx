b4e4221190b0c5af13a60bc9a002974935e9b9c9
crystian97 <icrystianveloso@gmail.com>
2023-01-30 11:27:49 -0300

    refactor:Trocando os campos do radix por styled

---
+++ src/Components/NewTransactionModal/index.tsx
@@ -0,0 +1,14 @@
+import * as Dialog from "@radix-ui/react-dialog";
+import { Content, Overlay } from "./styles";
+
+export function NewTransactionModal() {
+  return (
+    <Dialog.Portal>
+      <Overlay />
+      <Content>
+        <Dialog.Title>Nova Transação</Dialog.Title>
+        <Dialog.Close />
+      </Content>
+    </Dialog.Portal>
+  );
+}
